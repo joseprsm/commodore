12703b68c7702db9bd6049368498805ba5068e19
José Medeiros <joseprsm@gmail.com>
2022-09-12 10:55:54 +0100

    add subscription, spaces routers

--- commodore/app/main.py
+++ commodore/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from commodore.app.routers import users, items
+from commodore.app.routers import items, spaces, subscriptions, users
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(items.router)
+app.include_router(subscriptions.router)
+app.include_router(spaces.router)
