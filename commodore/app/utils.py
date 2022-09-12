9cbc4b3f1badde2c9a5c98596338412593b682c7
José Medeiros <joseprsm@gmail.com>
2022-09-12 10:56:19 +0100

    add validation methods

--- commodore/app/utils.py
+++ commodore/app/utils.py
@@ -9,6 +9,12 @@ def get_n_items(space):
     return db.document(space).get().to_dict()["n_items"]
 
 
+def id_exists(collection_id: str, value: str | int):
+    return db.collection(collection_id).document(str(value)).get().to_dict() is not None
 
-def get_n_items():
-    return db.get().to_dict()['n_items']
+
+def get_entrance_passes():
+    return [
+        doc.id
+        for doc in db.collection("items").where("recurring", "!=", "true").stream()
+    ]
