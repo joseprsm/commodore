4d2d723b4c52232ce742b72716999c90939c34b5
José Medeiros <joseprsm@gmail.com>
2022-09-12 10:51:37 +0100

    move db to __init__

---
+++ commodore/app/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+from google.cloud import firestore
+
+PROJECT_ID: str = os.environ.get("PROJECT_ID")
+
+# noinspection PyTypeChecker, PydanticTypeChecker
+db = firestore.Client(project=PROJECT_ID).collection("commodore")
--- commodore/app/utils.py
+++ commodore/app/utils.py
@@ -1,8 +1,13 @@
-from commodore.app.db import db
+from commodore.app import db
 
 
-def get_n_users():
-    return db.get().to_dict()['n_users']
+def get_n_users(space):
+    return db.document(space).get().to_dict()["n_users"]
+
+
+def get_n_items(space):
+    return db.document(space).get().to_dict()["n_items"]
+
 
 
 def get_n_items():
