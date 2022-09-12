e5dcfcf87fcef9d1b192c6236e80fe3cb50d90de
José Medeiros <joseprsm@gmail.com>
2022-09-12 18:48:37 +0100

    add item data class

---
+++ commodore/models/item.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+
+from commodore.models.base import IdentifiableMixin
+
+
+class Item(BaseModel, IdentifiableMixin):
+
+    name: str
+    price: float
+    description: str | None
+    recurring: bool | None
+    entrances: int | None
+
+    def create(self, space):
+        item_id = self.cardinality(space) + 1
+        self.get_document(space, item_id).set(self.__dict__)
+        self._get_space_document(space).update({"n_items": item_id})
+        return self
+
+    @classmethod
+    def get(cls, space: str, item_id: str | int):
+        cls.id_exists(space, item_id)
+        return Item(**cls.get_document(space, item_id).get().to_dict())
+
+    @classmethod
+    def update(cls, space: str, item_id: str | int, update: dict):
+        cls.id_exists(space, item_id)
+        doc = cls.get_document(space, item_id)
+        doc.update(update)
+        return Item(**doc.get().to_dict())
+
+    @classmethod
+    def delete(cls, space: str, item_id: str | int):
+        cls.id_exists(space, item_id)
+        cls.get_document(space, item_id).delete()
+        n_items = cls.cardinality(space) - 1
+        cls._get_space_document(space).update({"n_items": n_items})
+
+    @classmethod
+    def get_all(
+        cls,
+        space: str,
+        name: str = None,
+        min_price: float = None,
+        max_price: float = None,
+        recurring: bool = None,
+    ):
+        col = cls._get_collection(space)
+        col = col.where("name", "==", name) if name else col
+        col = col.where("price", ">=", min_price) if min_price else col
+        col = col.where("price", "<=", max_price) if max_price else col
+        col = col.where("recurring", "==", recurring) if recurring else col
+        return [Item(**doc.to_dict()) for doc in col.get()]
+
+    @staticmethod
+    def _get_collection_id() -> str:
+        return "items"
--- commodore/routers/items.py
+++ commodore/routers/items.py
@@ -1,10 +1,6 @@
-import json
-
 from fastapi import APIRouter
 
-from commodore import db
 from commodore.models import Item
-from commodore.utils import get_n_items, id_exists
 
 
 router = APIRouter()
@@ -12,11 +8,7 @@ router = APIRouter()
 
 @router.post("/{space}/items")
 async def create_item(space: str, item: Item):
-    it = json.loads(item.json())
-    item_id = get_n_items(space) + 1
-    db.document(space).collection("items").document(str(item_id)).set(it)
-    db.document(space).update({"n_items": item_id})
-    return it
+    return item.create(space)
 
 
 @router.get("/{space}/items")
@@ -27,38 +19,20 @@ async def get_items(
     max_price: float = None,
     recurring: bool = None,
 ):
-    col_ref = db.document(space).collection("items")
-
-    col = col_ref.where("name", "==", name) if name else col_ref
-    col = col.where("price", ">=", min_price) if min_price else col
-    col = col.where("price", "<=", max_price) if max_price else col
-    col = col.where("recurring", "==", recurring) if recurring else col
-
-    documents = col.get()
-    return {"items": [doc.to_dict() for doc in documents]}
+    return {"items": Item.get_all(space, name, min_price, max_price, recurring)}
 
 
 @router.get("/{space}/items/{item_id}")
 async def get_item(space: str, item_id: int):
-    if not id_exists(space, "items")(str(item_id)):
-        raise ValueError("`item_id` does not exist")
-    return db.document(space).collection("items").document(str(item_id)).get().to_dict()
+    return Item.get(space, item_id)
 
 
 @router.put("/{space}/items/{item_id}")
 async def update_item(space: str, item_id: int, update: dict):
-    if not id_exists(space, "items")(str(item_id)):
-        raise ValueError("`item_id` does not exist")
-    doc = db.document(space).collection("items").document(str(item_id))
-    doc.update(update)
-    return doc.get().to_dict()
+    return Item.update(space, item_id, update)
 
 
 @router.delete("/{space}/items/{item_id}")
 async def delete_item(space: str, item_id: int):
-    if not id_exists(space, "items")(str(item_id)):
-        raise ValueError("`item_id` does not exist")
-    db.document(space).collection("items").document(str(item_id)).delete()
-    n_items = get_n_items(space)
-    db.document(space).update({"n_items": n_items - 1})
+    Item.delete(space, item_id)
     return {"msg": f"item {item_id} deleted"}
