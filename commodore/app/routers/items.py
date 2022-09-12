65133387f5be83063c2d257eb17f5fa103befc56
José Medeiros <joseprsm@gmail.com>
2022-09-12 10:54:38 +0100

    add spaces to users, items routes

--- commodore/app/routers/items.py
+++ commodore/app/routers/items.py
@@ -2,33 +2,36 @@ import json
 
 from fastapi import APIRouter
 
-from commodore.app.db import db
+from commodore.app import db
 from commodore.app.models import Item
 from commodore.app.utils import get_n_items
 
 router = APIRouter()
-col_ref = db.collection("items")
 
 
-@router.post("/items")
-async def create_item(item: Item):
+@router.post("/{space}/items")
+async def create_item(space: str, item: Item):
     it = json.loads(item.json())
-    item_id = get_n_items() + 1
-    col_ref.document(str(item_id)).set(it)
-    db.update({"n_items": item_id})
+    item_id = get_n_items(space) + 1
+    db.document(space).collection('items').document(str(item_id)).set(it)
+    db.document(space).update({"n_items": item_id})
     return it
 
 
-@router.get("/items")
-async def get_items(name: str = None, min_price: float = None, max_price: float = None, recurring: bool = None):
+@router.get("/{space}/items")
+async def get_items(
+    space: str,
+    name: str = None,
+    min_price: float = None,
+    max_price: float = None,
+    recurring: bool = None,
+):
+    col_ref = db.document(space).collection('items')
+
     col = col_ref.where("name", "==", name) if name else col_ref
     col = col.where("price", ">=", min_price) if min_price else col
     col = col.where("price", "<=", max_price) if max_price else col
     col = col.where("recurring", "==", recurring) if recurring else col
 
     documents = col.get()
-    return {
-        "items": [
-            doc.to_dict() for doc in documents
-        ]
-    }
+    return {"items": [doc.to_dict() for doc in documents]}
--- commodore/app/routers/users.py
+++ commodore/app/routers/users.py
@@ -2,29 +2,25 @@ import json
 
 from fastapi import APIRouter
 
-from commodore.app.db import db
+from commodore.app import db
 from commodore.app.models import User
 from commodore.app.utils import get_n_users
 
 router = APIRouter()
-col_ref = db.collection("users")
 
 
-@router.get("/users")
-async def get_users(name: str = None):
+@router.get("/{space}/users")
+async def get_users(space: str, name: str = None):
+    col_ref = db.document(space).collection("users")
     col = col_ref.where("name", "==", name) if name else col_ref
     documents = col.get()
-    return {
-        "users": [
-            doc.to_dict() for doc in documents
-        ]
-    }
+    return {"users": [doc.to_dict() for doc in documents]}
 
 
-@router.post("/users")
-async def create_user(user: User):
+@router.post("/{space}/users")
+async def create_user(space: str, user: User):
     u = json.loads(user.json())
-    user_id = get_n_users() + 1
-    col_ref.document(str(user_id)).set(u)
-    db.update({"n_users": user_id})
+    user_id = get_n_users(space) + 1
+    db.document(space).collection("users").document(str(user_id)).set(u)
+    db.document(space).update({"n_users": user_id})
     return u
